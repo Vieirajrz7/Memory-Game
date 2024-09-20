6f1573ca35a14c49cd3bb8c259749d1772c74cbc
Pedro Henrique de Souza <23040@etecdracena.local>
2024-09-20 08:41:34 -0300

    first commit

---
+++ src/main/java/br/com/etecdracena/jogosdigitais/memorygame/MemoryGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.etecdracena.jogosdigitais.memorygame;
+
+/**
+ *
+ * @author 23040
+ */
+public class MemoryGame {
+
+    public static void main(String[] args) {
+        System.out.println("Memory Game!");
+    }
+}
